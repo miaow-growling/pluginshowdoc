858e1c1e0da68f180733ebf00cfa313509d6f548
何龙 <hel04@mingyuanyun.com>
2021-01-23 16:56:40 +0800

    增加图标

---
+++ src/icons/ShowDocIcons.java
@@ -0,0 +1,14 @@
+package icons;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+public final class ShowDocIcons {
+
+    public static final Icon SHOW_DOC = IconLoader.getIcon("/icons/showdoc.ico", ShowDocIcons.class);
+
+    private ShowDocIcons() {
+
+    }
+}
